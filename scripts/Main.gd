ab3442f4b08793e532e9c536a786c4b164bd6c61
Funkofrootloop <breamkillerx@gmail.com>
2025-07-24 17:48:15 -0700

    Create Main.gd

---
+++ scripts/Main.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+# Main scene script for Wardenlight
+# Initializes the game and will later handle transitions into Dreamvault dungeons.
+
+func _ready() -> void:
+    print("Welcome to Wardenlight!")
