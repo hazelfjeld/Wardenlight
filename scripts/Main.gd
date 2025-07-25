c6d194268da0cd531674df910d1787b1c089bb8c
Funkofrootloop <breamkillerx@gmail.com>
2025-07-24 18:08:16 -0700

    Delete scripts/Main.gd

--- scripts/Main.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-# Main scene script for Wardenlight
-# Initializes the game and will later handle transitions into Dreamvault dungeons.
-
-func _ready() -> void:
-    print("Welcome to Wardenlight!")
